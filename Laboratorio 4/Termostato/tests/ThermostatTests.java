61927e7f1a97f2e384d3698f15f6ad6a4bf89d7b
DavidChGA <100686200+DavidChGA@users.noreply.github.com>
2023-10-27 15:22:27 +0200

    Carpeta de Tests Creada

---
+++ Laboratorio 4/Termostato/tests/ThermostatTests.java
@@ -0,0 +1,22 @@
+import static org.junit.jupiter.api.Assertions.*;
+
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+class ThermostatTests {
+
+	@BeforeAll
+	static void setUpBeforeClass() throws Exception {
+	}
+
+	@BeforeEach
+	void setUp() throws Exception {
+	}
+
+	@Test
+	void testTurnHeaterOn() {
+		fail("Not yet implemented");
+	}
+
+}
