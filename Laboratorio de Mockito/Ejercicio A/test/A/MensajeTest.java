edf1a1e674e7b89d035ccf7d9bd7be0124915633
RubnMC <rubema06@ucm.es>
2023-11-17 14:06:03 +0100

    librerias mockito

---
+++ Laboratorio de Mockito/Ejercicio A/test/A/MensajeTest.java
@@ -0,0 +1,28 @@
+package A;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import static org.mockito.Mockito.*;
+
+class MensajeTest {
+	
+	@Test
+	void testsendMensaje() {
+		Cliente dummyCliente = mock(Cliente.class);
+		Template dummyTemplate = mock(Template.class);
+		MailServer dummyMailServer = mock(MailServer.class);
+		TemplateEngine dummyTE =  mock(TemplateEngine.class);
+		Mensaje mensaje = new Mensaje(dummyMailServer, dummyTE);
+		
+		
+		when(dummyCliente.getEmail()).thenReturn("HolaSOyUNMAIL");
+		when(dummyTE.preparaMensaje(any(Template.class), any(Cliente.class))).thenReturn("Mensaje");
+		
+		mensaje.sendMensaje(dummyCliente, dummyTemplate);
+		verify(dummyMailServer).send(dummyCliente.getEmail(),"Mensaje");
+	}
+
+}
