f4edf245a10d876ea0a3e6b5366ad8289246779d
DavidChGA <100686200+DavidChGA@users.noreply.github.com>
2023-11-21 16:57:00 +0100

    Nuevos nombres y cambio de .zip

---
+++ EjercicioMockitoEvaluable/EmisorFacturaTest.java
@@ -0,0 +1,47 @@
+package PA;
+
+import static org.mockito.Mockito.*;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+class EmisorFacturaTest {
+
+	PrinterService dummyPrinterService;
+	EmailService dummyEmailService;
+	Factura dummyFactura;
+	Cliente dummyCliente;
+	EmisorFactura emisorFactura;
+	
+	@BeforeEach
+	void setUp(){
+		dummyPrinterService = mock(PrinterService.class);
+		dummyEmailService = mock(EmailService.class);
+		dummyFactura = mock(Factura.class);
+		dummyCliente = mock(Cliente.class);
+		
+		emisorFactura = new EmisorFactura(dummyPrinterService, dummyEmailService);
+	}
+
+	@Test
+	void emitirFacturaEmail() {
+		when(dummyCliente.prefiereEmails()).thenReturn(true);
+		when(dummyCliente.getEmail()).thenReturn("unMail");
+		
+		emisorFactura.emitirFactura(dummyFactura, dummyCliente);
+		
+		verify(dummyEmailService).sendFactura(dummyFactura, "unMail");
+		verify(dummyPrinterService, never()).printFactura(dummyFactura);
+	}
+	
+	@Test
+	void emitirFacturaFisica() {
+		when(dummyCliente.prefiereEmails()).thenReturn(false);
+		
+		emisorFactura.emitirFactura(dummyFactura, dummyCliente);
+		
+		verify(dummyEmailService, never()).sendFactura(dummyFactura, "unMail");
+		verify(dummyPrinterService).printFactura(dummyFactura);
+	}
+
+}
